f526c1afbebf1ad3cfb0e20f3869153834a5113e
Nick Nagel <nick@good.co>
2017-06-22 10:58:54 -0400

    Initial setup of overflow adjust alg repo

---
+++ index.js
@@ -0,0 +1,57 @@
+function isOverflowed( box ) {
+    return( (box.scrollHeight > box.clientHeight) || (box.scrollWidth > box.clientWidth) );
+}
+
+function adjustFontsize () {
+
+    const BOX_LIST      = document.getElementsByClassName("test-class");
+    const MIN_FONT_SIZE = 6;
+    const STEP_SIZE     = 2;
+    var   size, newSize;
+
+    Array.prototype.forEach.call(
+        BOX_LIST,
+        function (box) {
+            box.style.fontSize = "";
+        }
+    );
+
+    newSize = size = parseInt( window.getComputedStyle(BOX_LIST[0],null).getPropertyValue("font-size") );
+
+    Array.prototype.forEach.call(
+        BOX_LIST,
+        function( box ) {
+            for ( ; size > MIN_FONT_SIZE; size -= STEP_SIZE ) {
+                if( isOverflowed(box) ) {
+                    box.style.fontSize = size + "px";
+                } else {
+                    size += STEP_SIZE;
+                    break;
+                }
+            }
+            newSize = Math.min( newSize, size );
+        }
+    );
+
+    Array.prototype.forEach.call(
+        BOX_LIST,
+        function (box) {
+            box.style.fontSize = newSize + "px";
+        }
+    );
+
+}
+
+
+( function() {
+    document.addEventListener( "DOMContentLoaded", function (event) { adjustFontsize(); } );
+    var resizeTimeout = null;
+    window.addEventListener( "resize", function (event) {
+        if( resizeTimeout !== null ) {
+            clearTimeout( resizeTimeout );
+        }
+        resizeTimeout = setTimeout( function () {
+            adjustFontsize();
+        }, 20);
+    } );
+} )();
